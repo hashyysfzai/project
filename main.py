6ba218798b3b4cfd35fc75977c7dd24bb8fe901b
hashyiqbal97 <31016009-hashyiqbal97@users.noreply.replit.com>
2024-06-24 10:38:01 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+name = str(input('Name: '))
+print('Name: ' + name)
+
+age = int(input('Age: '))
+print('Age: ', age)
+
+gender = str(input('Gender: '))
+print('Gender: ' + gender)
+
+if gender != "male" or "female":
+  print("invalid")
