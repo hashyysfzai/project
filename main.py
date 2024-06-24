41d82b2b7860d698a1e272171cac08861ce12ddc
hashyysfzai <121766398+hashyysfzai@users.noreply.github.com>
2024-06-24 11:36:25 +0000

    change

--- main.py
+++ main.py
@@ -4,10 +4,15 @@ print('Name: ' + name)
 age = int(input('Age: '))
 print('Age: ', age)
 
+
 gender = str(input('Gender: '))
-print('Gender: ' + gender)
+print('Gender: ' , gender)
 
-if gender != "male" or "female":
-  print("invalid")
+#if gender != "male" and gender != "female":
+ # gender = str(input('Gender: '))
 
-print('change')
+print(f'Hi {name}, we will now ask you some questions')
+
+cough = bool(input('Do you have a cough?'))
+if cough:
+  coughType = str('Is it a mucus, dry or chesty cough?')
