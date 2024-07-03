7549d7d53fa78d8483d7dc0e9256381e4711e3ce
hashyysfzai <121766398+hashyysfzai@users.noreply.github.com>
2024-07-03 13:57:14 +0000

    change

--- main.py
+++ main.py
@@ -19,6 +19,8 @@ if gender != 'male' and gender != 'female':
 else:
   pass
 
+
+
 #questions
 energy = int(
     input('On a scale from 1-10 how energetic are you feeling right now?'))
@@ -27,7 +29,7 @@ depressed = int(
     input('On a scale from 1-10 how depressed are you feeling right now?'))
 sleep = int(input('On a scale from 1-10 how is your sleep right now?'))
 water = int(input('How many glasses of water do you drink a day?'))
-diet = input('What is a rough percentage of your diet that is junk food')
+diet = int(input('What is a rough percentage of your diet that is junk food'))
 feel = int(input('On a scale from 1-10 how would YOU rate your lifestyle'))
 
 print('hmm interesting...')
@@ -39,6 +41,20 @@ print('C: Getting Stronger')
 print('D: Losing Weight')
 concern = str(input('Enter:'))
 
+if concern == 'A' and depressed > 3:
+    print('The reason for yur depression may be due to your lack of sleep')
+    print('-DO NOT: eat 3 hours before bed; drink 2 hours before bed; use devices 1 hour before bed')
+    print('if these do not work then feel free to contact us')
+
+if concern == 'A' and sleep > 7:
+    print('There doesnt seem to be an issue with your sleep, however if you would still like help the contact us')
+
+if concern == 'A' and energy > 7:
+    print('If you drink too much caffiene then cut down on it')
+
+if concern == 'B' and diet > 30:
+    print('')
+          
 #if concern == .... AND diet = ..... print......
 
 #if statements to be done
